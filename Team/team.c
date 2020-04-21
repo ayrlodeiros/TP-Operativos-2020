306da729351253245a076d69e437656106dd1062
Federicobunader <federico.bunader@gmail.com>
2020-04-21 17:48:40 -0300

    Arregle los hilos, falta el binario

--- Team/team.c
+++ Team/team.c
@@ -4,42 +4,43 @@
 //Creacion del hilo
 
 
-void* contar_hasta_100();
-void* saludar();
+void* sumar();
+void* restar();
 pthread_t entrenadores[3];
+int i=0;
 
 int main(void){
 
 
-	pthread_create(&(entrenadores[0]),NULL,contar_hasta_100,NULL);
+	pthread_create(&(entrenadores[0]),NULL,sumar,NULL);
 
-	pthread_create(&(entrenadores[1]),NULL,saludar,NULL);
+	pthread_create(&(entrenadores[1]),NULL,restar,NULL);
 
 
 	pthread_join(entrenadores[0],NULL);
 	pthread_join(entrenadores[1],NULL);
 
+	printf("El valor de i es : %d \n ",i);
 
 	return 0;
 }
 
-void* contar_hasta_100(){
+void* sumar(){
 
-	int i=0;
 
-	while(i<100){
-		printf("El valor de i es : %d",i);
+
+	while(i<10){
 		i++;
 	}
 	return NULL;
 }
 
-void* saludar(){
-	int i = 0;
+void* restar(){
 
-	while(i<100){
-		printf("Hola :%d",i);
-		i++;
+	int j=0;
+	while(j<10){
+		j++;
+		i--;
 	}
 	return NULL;
 }
