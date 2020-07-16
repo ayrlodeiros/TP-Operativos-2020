0673ee00ec13b74056c0ae791b54c0818f17815e
Federicobunader <federico.bunader@gmail.com>
2020-07-16 12:11:25 -0300

    Cambios en el Deploy Base

--- DeployBase.sh
+++ DeployBase.sh
@@ -53,18 +53,33 @@ cp -r tp-2020-1c-C-aprueba/ /home/utnso/workspace/tp-2020-1c-C-aprueba
 
 #FIN DE COPIAR CARPETA
 
+#INICIO DE GAMEBOY
+cd /home/utnso/workspace/tp-2020-1c-C-aprueba/gameboy-v2/Debug/
+
+make all
+
+cd ../src/resources/
+
+if test -e .gameBoy.config; then
+	rm .gameBoy.config
+fi
+#REVISAR -> No encuentro los parametros del gamecard(config en el archivo)
+echo -e "IP_BROKER=\nIP_TEAM=\nIP_GAMECARD=\nPUERTO_BROKER=\nPUERTO_TEAM=\nPUERTO_GAMECARD=\n" >> .gameBoy.config
+
+#FIN DE GAMEBOY
+
 #INICIO DE GAMECARD
 cd /home/utnso/workspace/tp-2020-1c-C-aprueba/gamecard-v2/Debug/
 
 make all
 
-cd ../Config/
+cd ../src/resources/
 
-if test -e .config; then
-	rm .config
+if test -e .gamecard.config; then
+	rm .gamecard.config
 fi
 #REVISAR -> No encuentro los parametros del gamecard(config en el archivo)
-echo -e "TIEMPO_DE_REINTENTO_CONEXION=10\nTIEMPO_DE_REINTENTO_OPERACION=5\nTIEMPO_RETARDO_OPERACION=1\nPUNTO_MONTAJE_TALLGRASS=/home/utnso/tallgrass-base/\nIP_BROKER=\nPUERTO_BROKER=\nID_MODULO=4\nIP_GAMECARD=\nPUERTO_GAMECARD=\n" >> .config
+echo -e "TIEMPO_DE_REINTENTO_CONEXION=10\nTIEMPO_DE_REINTENTO_OPERACION=5\nTIEMPO_RETARDO_OPERACION=1\nPUNTO_MONTAJE_TALLGRASS=/home/utnso/tallgrass-base/\nIP_BROKER=\nPUERTO_BROKER=\nID_MODULO=4\nIP_GAMECARD=\nPUERTO_GAMECARD=\n" >> .gamecard.config
 
 #FIN DE GAMECARD
 
@@ -76,7 +91,6 @@ make all
 
 cd ../../
 
-#cp team-v2/team-v2-FIFO
 cp team-v2/ team-v2-RR
 cp team-v2/ team-v2-SJF-SD
 cp team-v2/ team-v2-SJF-CD
@@ -87,37 +101,49 @@ cp team-v2/ team-v2-SJF-CD
 
 cd /home/utnso/workspace/tp-2020-1c-C-aprueba/team-v2/
 
-if test -e .config; then
-	rm .config
+cd /src/resources/
+
+if test -e .team.config; then
+	rm .team.config
 fi
 
-echo -e "POSICIONES_ENTRENADORES=[1|3,2|3,2|2]\nPOKEMON_ENTRENADORES=[Pikachu]\nOBJETIVOS_ENTRENADORES=[Pikachu|Squirtle,Pikachu|Gengar,Squirtle|Onix]\nTIEMPO_RECONEXION=30\nRETARDO_CICLO_CPU=5\nALGORITMO_PLANIFICACION=FIFO\nQUANTUM=0\nALPHA=0.5\nESTIMACION_INICIAL=5\nIP_BROKER=\nPUERTO_BROKER=\nLOG_FILE=" >> .config
+echo -e "POSICIONES_ENTRENADORES=[1|3,2|3,2|2]\nPOKEMON_ENTRENADORES=[Pikachu]\nOBJETIVOS_ENTRENADORES=[Pikachu|Squirtle,Pikachu|Gengar,Squirtle|Onix]\nTIEMPO_RECONEXION=30\nRETARDO_CICLO_CPU=5\nALGORITMO_PLANIFICACION=FIFO\nQUANTUM=0\nALPHA=0.5\nESTIMACION_INICIAL=5\nIP_BROKER=\nPUERTO_BROKER=\nLOG_FILE=" >> .team.config
 
 #Configuro team con RR -> Quantum = 2
 
-cd ../team-v2-RR/
+cd ../../../team-v2-RR/
 
-if test -e .config; then
-	rm .config
+cd /src/resources/
+
+if test -e .team.config; then
+	rm .team.config
 fi
 
-echo -e "POSICIONES_ENTRENADORES=[1|3,2|3,2|2]\nPOKEMON_ENTRENADORES=[Pikachu]\nOBJETIVOS_ENTRENADORES=[Pikachu|Squirtle,Pikachu|Gengar,Squirtle|Onix]\nTIEMPO_RECONEXION=30\nRETARDO_CICLO_CPU=5\nALGORITMO_PLANIFICACION=RR\nQUANTUM=2\nALPHA=0.5\nESTIMACION_INICIAL=5\nIP_BROKER=\nPUERTO_BROKER=\nLOG_FILE=" >> .config
+echo -e "POSICIONES_ENTRENADORES=[1|3,2|3,2|2]\nPOKEMON_ENTRENADORES=[Pikachu]\nOBJETIVOS_ENTRENADORES=[Pikachu|Squirtle,Pikachu|Gengar,Squirtle|Onix]\nTIEMPO_RECONEXION=30\nRETARDO_CICLO_CPU=5\nALGORITMO_PLANIFICACION=RR\nQUANTUM=2\nALPHA=0.5\nESTIMACION_INICIAL=5\nIP_BROKER=\nPUERTO_BROKER=\nLOG_FILE=" >> .team.config
 
 #Configuro team con SJF - SD
 
-if test -e .config; then
-	rm .config
+cd ../../../team-v2-SJF-SD/
+
+cd /src/resources/
+
+if test -e .team.config; then
+	rm .team.config
 fi
 
-echo -e "POSICIONES_ENTRENADORES=[1|3,2|3,2|2]\nPOKEMON_ENTRENADORES=[Pikachu]\nOBJETIVOS_ENTRENADORES=[Pikachu|Squirtle,Pikachu|Gengar,Squirtle|Onix]\nTIEMPO_RECONEXION=30\nRETARDO_CICLO_CPU=5\nALGORITMO_PLANIFICACION=SFJ-SD\nQUANTUM=0\nALPHA=0.5\nESTIMACION_INICIAL=5\nIP_BROKER=\nPUERTO_BROKER=\nLOG_FILE=" >> .config
+echo -e "POSICIONES_ENTRENADORES=[1|3,2|3,2|2]\nPOKEMON_ENTRENADORES=[Pikachu]\nOBJETIVOS_ENTRENADORES=[Pikachu|Squirtle,Pikachu|Gengar,Squirtle|Onix]\nTIEMPO_RECONEXION=30\nRETARDO_CICLO_CPU=5\nALGORITMO_PLANIFICACION=SFJ-SD\nQUANTUM=0\nALPHA=0.5\nESTIMACION_INICIAL=5\nIP_BROKER=\nPUERTO_BROKER=\nLOG_FILE=" >> .team.config
 
 #Configuro team con SJF - CD
 
-if test -e .config; then
-	rm .config
+cd ../../../team-v2-SJF-CD/
+
+cd /src/resources/
+
+if test -e .team.config; then
+	rm .team.config
 fi
 
-echo -e "POSICIONES_ENTRENADORES=[1|3,2|3,2|2]\nPOKEMON_ENTRENADORES=[Pikachu]\nOBJETIVOS_ENTRENADORES=[Pikachu|Squirtle,Pikachu|Gengar,Squirtle|Onix]\nTIEMPO_RECONEXION=30\nRETARDO_CICLO_CPU=5\nALGORITMO_PLANIFICACION=SFJ-CD\nQUANTUM=0\nALPHA=0.5\nESTIMACION_INICIAL=5\nIP_BROKER=\nPUERTO_BROKER=\nLOG_FILE=" >> .config
+echo -e "POSICIONES_ENTRENADORES=[1|3,2|3,2|2]\nPOKEMON_ENTRENADORES=[Pikachu]\nOBJETIVOS_ENTRENADORES=[Pikachu|Squirtle,Pikachu|Gengar,Squirtle|Onix]\nTIEMPO_RECONEXION=30\nRETARDO_CICLO_CPU=5\nALGORITMO_PLANIFICACION=SFJ-CD\nQUANTUM=0\nALPHA=0.5\nESTIMACION_INICIAL=5\nIP_BROKER=\nPUERTO_BROKER=\nLOG_FILE=" >> .team.config
 #FIN DE TEAM
 
 #INICIO BROKER
@@ -139,31 +165,37 @@ cp broker-v2/ broker-v2-BUDDY-SYSTEM
 
 cd /home/utnso/workspace/tp-2020-1c-C-aprueba/broker-v2/
 
-if test -e .config; then
-	rm .config
+cd /src/resources/
+
+if test -e .broker.config; then
+	rm .broker.config
 fi
 
-echo -e "TAMANO_MEMORIA=64\nTAMANO_MINIMO_PARTICION=4\nALGORITMO_MEMORIA=PARTICIONES\nALGORITMO_REEMPLAZO=FIFO\nALGORITMO_PARTICION_LIBRE=FF\nIP_BROKER=\nPUERTO_BROKER=\nFRECUENCIA_COMPACTACION=10\nLOG_FILE=\n">> .config
+echo -e "TAMANO_MEMORIA=64\nTAMANO_MINIMO_PARTICION=4\nALGORITMO_MEMORIA=PARTICIONES\nALGORITMO_REEMPLAZO=FIFO\nALGORITMO_PARTICION_LIBRE=FF\nIP_BROKER=\nPUERTO_BROKER=\nFRECUENCIA_COMPACTACION=10\nLOG_FILE=\n">> .broker.config
 
 #Configuro broker compactacion Particiones Dinamicas
 
-cd ../broker-v2-COMPACTACION-PD/
+cd ../../../broker-v2-COMPACTACION-PD/
 
-if test -e .config; then
-	rm .config
+cd /src/resources/
+
+if test -e .broker.config; then
+	rm .broker.config
 fi
 
-echo -e "TAMANO_MEMORIA=64\nTAMANO_MINIMO_PARTICION=4\nALGORITMO_MEMORIA=PARTICIONES\nALGORITMO_REEMPLAZO=FF\nALGORITMO_PARTICION_LIBRE=FF\nIP_BROKER=\nPUERTO_BROKER=\nFRECUENCIA_COMPACTACION=1\nLOG_FILE=\n">> .config
+echo -e "TAMANO_MEMORIA=64\nTAMANO_MINIMO_PARTICION=4\nALGORITMO_MEMORIA=PARTICIONES\nALGORITMO_REEMPLAZO=FF\nALGORITMO_PARTICION_LIBRE=FF\nIP_BROKER=\nPUERTO_BROKER=\nFRECUENCIA_COMPACTACION=1\nLOG_FILE=\n">> .broker.config
 
 #Configuro broker compactacion Particiones Dinamicas
 
-cd ../broker-v2-BUDDY-SYSTEM/
+cd ../../../broker-v2-BUDDY-SYSTEM/
 
-if test -e .config; then
-	rm .config
+cd /src/resources/
+
+if test -e .broker.config; then
+	rm .broker.config
 fi
 
-echo -e "TAMANO_MEMORIA=64\nTAMANO_MINIMO_PARTICION=4\nALGORITMO_MEMORIA=BS\nALGORITMO_REEMPLAZO=FIFO\nALGORITMO_PARTICION_LIBRE=FF\nIP_BROKER=\nPUERTO_BROKER=\nFRECUENCIA_COMPACTACION=1\nLOG_FILE=\n">> .config
+echo -e "TAMANO_MEMORIA=64\nTAMANO_MINIMO_PARTICION=4\nALGORITMO_MEMORIA=BS\nALGORITMO_REEMPLAZO=FIFO\nALGORITMO_PARTICION_LIBRE=FF\nIP_BROKER=\nPUERTO_BROKER=\nFRECUENCIA_COMPACTACION=1\nLOG_FILE=\n">> .broker.config
 
 #FIN DE BROKER
 
