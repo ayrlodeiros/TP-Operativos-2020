71bcf861d1d65ea36f904ffe8ca92a48da69468c
AgustinAguilera <agustinaguileramontiel@gmail.com>
2020-07-20 16:03:01 -0300

    cambio_log

--- pruebas/new_pokemon_post_team.sh
+++ pruebas/new_pokemon_post_team.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-./gameboy BROKER APPEARED_POKEMON Onix 2 8 5
-./gameboy BROKER APPEARED_POKEMON Gengar 6 6 6
-./gameboy BROKER APPEARED_POKEMON Squirtle 5 5 7
+./gameboy BROKER NEW_POKEMON Onix 2 8 5
+./gameboy BROKER NEW_POKEMON Gengar 6 6 6
+./gameboy BROKER NEW_POKEMON Squirtle 5 5 7
 
-./gameboy BROKER APPEARED_POKEMON Umbreon 10 6 8
-./gameboy BROKER APPEARED_POKEMON Espeon 7 1 9
-./gameboy BROKER APPEARED_POKEMON Vaporeon 4 10 10
+./gameboy BROKER NEW_POKEMON Umbreon 10 6 8
+./gameboy BROKER NEW_POKEMON Espeon 7 1 9
+./gameboy BROKER NEW_POKEMON Vaporeon 4 10 10
